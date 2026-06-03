f1cc769da6e4ef9b71c500405dcb255f997a7b30
prashna2025 <prashnaraut32@gmail.com>
2026-06-03 22:59:07 +0545

    6th

--- script.js
+++ script.js
@@ -8,18 +8,57 @@ const generateEL = document.getElementById('generate')
 const clipboardEL = document.getElementById('clipboard')
 
 
+const randomFunc = {
+    lower: getRandomLowercase,
+    upper: getRandomUppercase,
+    number: getRandomNumbers,
+    symbols: getRandomSymbols
+}
+
+generateEL.addEventListener('click', () => {
+    const length = +lengthEL.Value
+    const hasLower = lowercaseEL.checked
+    const hasUpper = uppercaseEL.checked;
+    const hasNumer = numbersEL.checked;
+    const hasSymbol = symbolsEL.checked;
 
 
 
-//To generate lowercase letters you have to between 97 to 122 char-code
-function getRandomLowercase(){
-    console.log(String.fromCharCode(Math.floor(Math.random()*26)+97))
+    resultEL.innerText = generatePassword(hasLower , hasUpper, hasNumber, hasSymbol , length)
 
+})
+
+function generatePassword(lower,upper,number,symbol, length){
+    let generatePassword = ''
+    
+    let typeCount = lower + upper + number +symbol
+
+    console.log(type)
 }
 
 //To generate lowercase letters you have to between 97 to 122 char-code
-function getRandomLowercase(){
-    console.log(String.fromCharCode(Math.floor(Math.random()*26)+97))
+function getRandomLowercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 
 }
+
+//To generate uppercase letters you have to between 65  to 90 char-code
+function getRandomUppercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+
+}
+
+//To generate Numbers you have to between 48 to 57 char-code
+function getRandomNumbers() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+
+}
+
+function getRandomSymbols() {
+    const symbols = "!@#$%^&*(){}[]=<>/,.";
+
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+
 
