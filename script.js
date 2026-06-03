f97e62a9befe2b29423e70d7b645fbb2f2d5a21c
prashna2025 <prashnaraut32@gmail.com>
2026-06-03 20:10:37 +0545

    5th

---
+++ script.js
@@ -0,0 +1,25 @@
+const resultEL = document.getElementById('result')
+const lengthEL = document.getElementById('length')
+const uppercaseEL = document.getElementById('uppercase')
+const lowercaseEL = document.getElementById('lowercase')
+const numbersEL = document.getElementById('numbers')
+const symbolsEL = document.getElementById('symbols')
+const generateEL = document.getElementById('generate')
+const clipboardEL = document.getElementById('clipboard')
+
+
+
+
+
+//To generate lowercase letters you have to between 97 to 122 char-code
+function getRandomLowercase(){
+    console.log(String.fromCharCode(Math.floor(Math.random()*26)+97))
+
+}
+
+//To generate lowercase letters you have to between 97 to 122 char-code
+function getRandomLowercase(){
+    console.log(String.fromCharCode(Math.floor(Math.random()*26)+97))
+
+}
+
